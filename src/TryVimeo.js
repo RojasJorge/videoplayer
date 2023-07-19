68d391a2e206afff24a0d900c49b15b2d375a2f2
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2023-07-18 19:18:55 -0600

    feat: only fetch data

--- src/TryVimeo.js
+++ src/TryVimeo.js
@@ -1,8 +1,8 @@
 import Vimeo from "@u-wave/react-vimeo";
 
 const TryVimeo = () => {
-  const onEnd = async (data) => {
-    await fetch("https://webhook.site/ad4e8c43-f29b-4804-bba8-e811288f9df8", {
+  const onEnd = (data) => {
+    fetch("https://webhook.site/ad4e8c43-f29b-4804-bba8-e811288f9df8", {
       method: "POST",
       body: JSON.stringify(data),
     });
