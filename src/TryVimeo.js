94f2181e7c495b11ab12871e769c320aa3d08f99
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2023-07-18 21:51:35 -0600

    feat: add onPause event

--- src/TryVimeo.js
+++ src/TryVimeo.js
@@ -2,7 +2,22 @@ import Vimeo from "@u-wave/react-vimeo";
 import axios from "axios";
 
 const TryVimeo = () => {
-  const onEnd = async (data, vid) => {
+  const onEnd = async (data) => {
+    try {
+      const req = await axios.post(
+        "https://webhook.site/ad4e8c43-f29b-4804-bba8-e811288f9df8",
+        JSON.stringify(data)
+      );
+
+      const resp = await req.data();
+
+      console.log("response from webhook ==>>", resp);
+      return;
+    } catch (error) {
+      console.log("error", new Error(error));
+    }
+  };
+  const onPause = async (data) => {
     try {
       const req = await axios.post(
         "https://webhook.site/ad4e8c43-f29b-4804-bba8-e811288f9df8",
@@ -26,6 +41,7 @@ const TryVimeo = () => {
         autoplay={true}
         controls={false}
         onPlaybackRateChange={() => console.log("change playback")}
+        onPause={onPause}
         // keyboard={false}
         showPortrait
         showByline
