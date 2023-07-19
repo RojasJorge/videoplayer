c2a6ba5b69f10cb6827e7e253ef8d3148e385105
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2023-07-18 19:44:10 -0600

    feat: handle webhook

--- src/TryVimeo.js
+++ src/TryVimeo.js
@@ -2,12 +2,20 @@ import Vimeo from "@u-wave/react-vimeo";
 import axios from "axios";
 
 const TryVimeo = () => {
-  const onEnd = (data, vid) => {
-    console.log(data, vid);
-    axios.post(
-      "https://webhook.site/ad4e8c43-f29b-4804-bba8-e811288f9df8",
-      JSON.stringify(data)
-    );
+  const onEnd = async (data, vid) => {
+    try {
+      const req = await axios.post(
+        "https://webhook.site/ad4e8c43-f29b-4804-bba8-e811288f9df8",
+        JSON.stringify(data)
+      );
+
+      const resp = await req.data();
+
+      console.log("response from webhook ==>>", resp);
+      return;
+    } catch (error) {
+      console.log("error", new Error(error));
+    }
   };
   return (
     <>
