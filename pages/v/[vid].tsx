7a9d0fe61a65f46e56bb6e55227acb4792f23282
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2023-07-19 23:37:20 -0600

    feat: add changes

--- pages/v/[vid].tsx
+++ pages/v/[vid].tsx
@@ -1,35 +1,59 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 // import Vimeo from "@u-wave/react-vimeo";
 // import Vimeo from "react-vimeo";
-// import ReactPlayer from "react-player/vimeo";
+import ReactPlayer from "react-player/lazy";
 
-import { default as _ReactPlayer } from "react-player";
-import { ReactPlayerProps } from "react-player/types/lib";
-const ReactPlayer = _ReactPlayer as unknown as React.FC<ReactPlayerProps>;
+// import { default as _ReactPlayer } from "react-player";
+// import { ReactPlayerProps } from "react-player/types/lib";
+// const ReactPlayer = _ReactPlayer as unknown as React.FC<ReactPlayerProps>;
 
 const SingleVideoPage = ({ vid }) => {
   const [loaded, setLoaded] = useState(false);
+
   const [playing, setPlaying] = useState(false);
-  const eventHandler = async (e: any, type: string) => {
-    e.type = type;
+
+  const playPauseTrigger = useRef<HTMLInputElement>(null);
+
+  const handlePause = async (e: void) => {
+    e.type = "onPause";
+
+    try {
+      const req = await axios.post(
+        process.env.NEXT_PUBLIC_WEBHOOK,
+        JSON.stringify(e)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleReady = (e: any) => {
+    console.log("IS READY", e, playPauseTrigger);
+    setPlaying(true);
+    playPauseTrigger.current.click();
+  };
+
+  const eventHandler = async (e: any) => {
+    // e.type = type;
+
+    console.log("the type is-->>", e);
 
     // try {
     //   const req = await axios.post(
     //     process.env.NEXT_PUBLIC_WEBHOOK,
     //     JSON.stringify(e)
     //   );
-
-    //   return;
     // } catch (error) {
     //   console.log("error", new Error(error));
-    // }
+    // {
   };
 
   useEffect(() => {
     setLoaded(true);
-    setPlaying(true);
-  }, [playing]);
+    playPauseTrigger.current.click();
+  }, []);
+
   return (
     <>
       {/* <button id="doplay">INICIAR / PAUSAR</button> */}
@@ -38,14 +62,32 @@ const SingleVideoPage = ({ vid }) => {
           url={`https://player.vimeo.com/video/${vid}`}
           width="100vw"
           height="100vh"
-          controls={false}
           playing={playing}
+          onPause={handlePause}
+          onReady={handleReady}
         />
       ) : (
-        "cargando"
+        // <Vimeo
+        //   video={vid}
+        //   // videoId={vid}
+        //   width="100%"
+        //   height="100vh"
+        //   controls={false}
+        //   playsInline
+        //   autoplay
+        //   responsive
+        // />
+        "Cargando contenido..."
       )}
 
-      <button onClick={() => setPlaying(!playing)}>Play/Pause</button>
+      <div
+        ref={playPauseTrigger}
+        className="play-pause-custom"
+        onClick={() => {
+          console.log("clicked!!!");
+          setPlaying(!playing);
+        }}
+      ></div>
     </>
   );
 };
