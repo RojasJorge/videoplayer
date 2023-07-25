4c541a2bd3881e936fca5a2fe24b8af9e93c7de4
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2023-07-24 20:30:22 -0600

    feat: add html native video

---
+++ pages/v3/[vid].tsx
@@ -0,0 +1,76 @@
+import { useRef, useState, useEffect } from "react";
+
+const CustomPlayer = ({ vid, query }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const [muted, setMuted] = useState(true);
+
+  const handleClick = () => {
+    const video = videoRef.current;
+
+    if (!video.paused && muted) {
+      video.currentTime = 0;
+      setMuted(false);
+      video.play();
+    } else if (!video.paused && !muted) {
+      video.pause();
+    } else {
+      video.play();
+    }
+  };
+
+  const handlePauseEvent = (e: any) => {
+    const data = {
+      ...query,
+      type: e.type,
+      currentTime: videoRef.current.currentTime,
+    };
+
+    console.log("ON PAUSE -->>", data);
+  };
+
+  const handleEndEvent = (e: any) => {
+    const data = {
+      ...query,
+      type: e.type,
+      currentTime: videoRef.current.currentTime,
+    };
+
+    console.log("ON ENDED -->>", data);
+  };
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    video.play();
+  }, [muted]);
+
+  return (
+    <>
+      <video
+        className="native-video"
+        ref={videoRef}
+        onEnded={handleEndEvent}
+        onPause={handlePauseEvent}
+        onClick={handleClick}
+        muted={muted}
+        controls={false}
+        preload="true"
+        autoPlay={true}
+      >
+        <source src={`/${vid}.mp4`} type="video/mp4" />
+      </video>
+    </>
+  );
+};
+
+export const getServerSideProps = (ctx) => {
+  return {
+    props: {
+      vid: ctx.params.vid,
+      query: ctx.query,
+    },
+  };
+};
+
+export default CustomPlayer;
